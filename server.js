471082e81c36edef4fd4ba4da80d9818e42eb57b
tawahpeggy <tawahpeggy98@gmail.com>
2019-10-29 21:23:37 -0700

    deploying v

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path=require('path');
 const app=express();
 
 
-app.use(express.static('./dist/product-catalog'));
+app.use(express.static('./dist/angular'));
 //path location strategy
 app.get ('/*', function(req, res){
-    res.sendFile(path.join(__dirname +'/dist/product-catalog/index.html'));
+    res.sendFile(path.join(__dirname +'/dist/angular/index.html'));
 });
 app.listen(process.env.PORT || 8080);
